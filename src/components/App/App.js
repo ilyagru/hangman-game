196d24713164bc4cfc5e82d92d26910c6d51edb1
Ilya Gruzhevski <>
2017-07-12 13:58:02 +0300

    added https

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -61,7 +61,7 @@ class App extends Component {
     // HELPERS
 
     async fetchRandomWord() {
-        const response = await fetch('http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=11&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
+        const response = await fetch('https://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=11&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5');
         const json = await response.json();
         const word = json.word.toLowerCase();
 
